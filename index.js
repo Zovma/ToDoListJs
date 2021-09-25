fba2229da2c09a17e9335a9354379e49d969179c
Zovma <dim.borzov@gmail.com>
2021-09-26 00:39:57 +0300

    second connit

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
+
+let object = [
+    { id: 0, value: 'Построить дом' },
+    { id: 1, value: 'Посадить дерево' },
+    { id: 2, value: 'Вырастьть сына' }
+]
+$(window).ready(createLi(object))
+let count = 3
 $('.btn').bind('click', function () {
-    var jsItem = $('<div>'+ inputValue() +'<button>удалить</button></div>');  
-    console.log(jsItem);
-    $('.list').append(jsItem);
-    $('.input').val('') 
+    if (inputValue() !== '') {
+        object.push({ id: count, value: '' + inputValue() })
+        console.log(object);
+        $('.input').val('')
+        count++
+        $('*li').remove()
+        createLi(object)
+
+    }
 });
+
+function createLi(object) {
+    for (data of object) {
+        $('.list').append('<li id:' + data.id + '> ' + data.value + '<button>Удалить</button></li>')
+    }
+
+}
+
+function del(object, id) {
+    for (let i = 0; i < object.lenght; i++) {
+        console.log(object[i]);
+        if (object[i].id == id) {
+            delete object[i]
+            console.log('work');
+            break
+
+        }
+    }
+}
+
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
 
 
 function inputValue() {
-    var newList = $('<ul><li>Item1</li><li>Item2</li></ul>');
-    console.log(newList.html());
     return ($('.input').val());
 }
 
